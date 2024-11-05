66af2e9ea182ead0beda0fedcc7c38ad8361fcc0
A1esha <alexleha2004@mail.ru>
2024-11-06 00:38:35 +0100

    init spring boot app

---
+++ src/main/java/com/tis/dbf/DbfApplication.java
@@ -0,0 +1,13 @@
+package com.tis.dbf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbfApplication.class, args);
+    }
+
+}
