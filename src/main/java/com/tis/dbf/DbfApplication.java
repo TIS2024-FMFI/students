61dd8cdf9852f50b1724e1a0409e990e60ed0297
A1esha <alexleha2004@mail.ru>
2024-12-03 20:01:48 +0100

    create connection to server and download data

--- src/main/java/com/tis/dbf/DbfApplication.java
+++ src/main/java/com/tis/dbf/DbfApplication.java
@@ -2,6 +2,7 @@ package com.tis.dbf;
 
 import com.tis.dbf.model.Students;
 import com.tis.dbf.model.Subjects;
+import com.tis.dbf.service.ServerConnection;
 import com.tis.dbf.service.XMLParsingServes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,16 +15,22 @@ public class DbfApplication implements CommandLineRunner {
     @Autowired
     private XMLParsingServes xmlParsingServes;
 
+    @Autowired
+    private ServerConnection serverConnection;
+
     public static void main(String[] args) {
         SpringApplication.run(DbfApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception{
-        Subjects subjects = xmlParsingServes.parseSubjectsXml("example.xml");
-        Students students = xmlParsingServes.parseStudents("studentsExample.xml");
+
+//        Students students = xmlParsingServes.parseStudents("studentsExample.xml");
+//        System.out.println(students);
+
+        serverConnection.downloadFilesFromServer();
+        Subjects subjects = xmlParsingServes.parseSubjectsXml("predmety.xml");
         System.out.println(subjects);
-        System.out.println(students);
     }
 
 }
